b2f37cc34678937d0cd9bdebc2312fc7eab9ea63
DoanVietThanh <doanvietthanhhs@gmail.com>
2024-11-04 07:38:50 +0700

    build app

---
+++ android/app/src/main/java/senbox/vercel/app/MainActivity.java
@@ -0,0 +1,5 @@
+package senbox.vercel.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vercel.senbox',
+  appName: 'senbox',
+  webDir: 'out',
+};
+
+export default config;
+
+// out -> dùng cho nextjs
+// public -> dùng cho Vue, Angular
